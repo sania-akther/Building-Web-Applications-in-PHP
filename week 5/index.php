7ddb6c1dd89cd5bfc68bfb7905da83f6ba888111
sania-akther <mostsaniaakthersara@gmail.com>
2020-10-04 17:03:45 +0600

    upload files

---
+++ week 5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Most Sania akther sara PHP</title>
+</head>
+<body>
+<h1>Most Sania akther sara PHP</h1>
+The SHA256 hash of "Most Sania akther sara" is
+
+<?php
+ $txt = "eddcba64ca0a364fab12becff8d956c22c0fa2cbf61298c66543b04ea7974f43";
+ echo "$txt!"; 
+?>
+<pre>
+ASCII ART:
+  ___ 
+ / __|
+ \__ \
+ |___/
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
+</html>
---
+++ week 6/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Most Sania akther sara</title>
+</head>
+<body>
+<h1>Welcome to my guessing game</h1>
+<p>
+<?php
+$correctnumber=25; //change this number //
+if(isset($_GET['guess']))
+{
+  if (is_numeric($_GET['guess'])===FALSE)
+ {
+echo "Your guess is not a number";
+}
+else if ($_GET['guess']<$correctnumber)
+ {
+echo "Your guess is too low";
+}
+else if ($_GET['guess']>$correctnumber)
+ {
+echo "Your guess is too high";
+}
+else if ($_GET['guess']==$correctnumber)
+ {
+echo "Congratulations - You are right";
+}
+}
+else
+{
+echo "Missing guess parameter <br> Your guess is too short";
+}
+?>
+</p>
+    
+</body>
+</html>
